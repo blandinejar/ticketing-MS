774d5ca153afc1aeecb9641046aaa6e5ab9314e3
blandinejar <blandine.jarysta@gmail.com>
2022-07-07 15:57:05 +0200

    error handling section done

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -7,6 +8,7 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 
 import { errorHandler } from './middlewares/error-handling';
+import { NotFoundError } from './errors/not-found-error';
 
 try { 
 
@@ -17,6 +19,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async () => {
+    throw new NotFoundError(); 
+});
 
 app.use(errorHandler);
 
