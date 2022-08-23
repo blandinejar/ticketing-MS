0bbd3974d12656a45cb00e141fcbe6f440295eb0
blandinejar <blandine.jarysta@gmail.com>
2022-08-23 17:41:12 +0200

    authentication but jwt_key not found

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection.error';
+import { BadRequestRerror } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -20,13 +23,28 @@ router.post('/api/users/signup', [
         throw new RequestValidationError(errors.array());
     }
 
-    // const { email, password } = req.body;
-
-    console.log('Creating a user');
-    throw new DatabaseConnectionError();
+    const { email, password } = req.body;
     
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+        throw new BadRequestRerror('Email in use');
+    }
+
+    const user = User.build({email, password});
+    await user.save();
+
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, 
+    process.env.JWT_KEY!);
+
+    req.session = {
+        jwt: userJwt
+    };
+
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
